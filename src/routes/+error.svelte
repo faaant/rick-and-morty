d08dc1e2a399b0467848b10317c331ecbfa99124
faaant <illiaeducation@gmail.com>
2025-01-19 23:19:22 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+</script>
+
+<section>
+  <div>
+    <h1>Ooops... Smth went wrong</h1>
+    <h3>
+      <button onclick={() => goto('/')}>Go back!</button>
+    </h3>
+  </div>
+</section>
+
+<style>
+  section {
+    min-height: calc(100vh - 4rem);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button {
+    height: 60px;
+  }
+</style>
