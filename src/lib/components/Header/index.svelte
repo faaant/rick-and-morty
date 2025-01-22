d27dd0d51fa2db85443d3cf226d8db443254e6bc
faaant <illiaeducation@gmail.com>
2025-01-22 18:04:43 +0200

    Add link to home page on logo

--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -3,15 +3,14 @@
 </script>
 
 <header>
-  <Logo />
-  <h1>Rick and Morty</h1>
+  <a href="/">
+    <Logo />
+    <h1>Rick and Morty</h1>
+  </a>
 </header>
 
 <style>
   header {
-    display: flex;
-    align-items: center;
-    min-height: 4rem;
     background-color: var(--pico-color-slate-150);
     box-shadow: var(--pico-box-shadow);
     position: sticky;
@@ -22,5 +21,12 @@
   h1 {
     margin: 0;
     color: var(--pico-color-grey-700);
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    min-height: 4rem;
+    text-decoration: none;
   }
 </style>
